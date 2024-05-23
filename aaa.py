9a2235538fc82fa7a2477e851d5de9fc8e2ed032
Alant7799 <alantoulouse1@gmail.com>
2024-05-23 00:59:58 -0300

    refactor, added gitignore file

--- aaa.py
+++ aaa.py
@@ -5,7 +5,7 @@ import requests
 
 headers = {'resolution':'720','User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36'}
 
-r = requests.get(url, headers=headers, stream=True)
+r = requests.get(url)
 
 soup = BeautifulSoup(r.text, 'html.parser')
 
--- main.py
+++ main.py
@@ -2,58 +2,100 @@ import pandas as pd
 from bs4 import BeautifulSoup
 import json
 import requests
-import pyspark
 from pyspark.sql import SparkSession
 from pyspark import SparkContext
 
-
-sc = SparkContext.getOrCreate()
-spark = SparkSession.builder.appName("Pandas to Spark").getOrCreate()
-
 local_url = "excel_files\Muestra Anual LN+.xlsx"
-processed_url = local_url.replace("\\","/")
 
-pd_df = pd.read_excel(io=processed_url, skiprows=1, usecols="A:C")
-pd_df.columns = ["week","program","link"]
+"""recibe url de un archivo excel donde se consumen las 3 primeras columnas"""
+def extract(local_url):
 
-spark_df = spark.createDataFrame(pd_df)
+    sc = SparkContext.getOrCreate()
+    spark = SparkSession.builder.appName("Pandas to Spark").getOrCreate()
 
-df_table_view = spark_df.createOrReplaceTempView("links")
+    processed_url = local_url.replace("\\","/")
 
-df_select = spark.sql("SELECT regexp_replace(week, '[0-9]*[.] Semana del ', '') as week , program, link FROM links where link != 'NaN'")
+    pd_df = pd.read_excel(io=processed_url, skiprows=1, usecols="A:C")
+    pd_df.columns = ["week","program","link"]
 
-links_list = df_select.toPandas().values.tolist()
+    spark_df = spark.createDataFrame(pd_df)
 
-dict = {}
+    df_table_view = spark_df.createOrReplaceTempView("links")
 
-for row in links_list:
-    week, program, link = row[0], row[1], row[2]
+    df_select = spark.sql("SELECT regexp_replace(week, '[0-9]*[.] Semana del ', '') as week , program, link FROM links where link != 'NaN'")
 
-    week = week.replace(" al", " -")
-    week = week.replace(" de ", " ")
+    return df_select.toPandas().values.tolist()
 
-    program = program.replace('"', '')
+""" recibe una lista de listas [[week, program, link]...], la limpia"""
+def clean_list(links_list):
 
-    dict[week + " " + program] = link
-    print(dict)
+    dict = {}
 
-headers = {'resolution':'720','User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36'}
+    for row in links_list:
 
-for key in dict.keys():
+        week, program, link = row[0], row[1], row[2]
 
-    r = requests.get(dict[key], headers=headers, stream=True)
+        week = week.replace(" al", " -")
+        week = week.replace(" de ", " ")
 
-    soup = BeautifulSoup(r.text, 'html.parser')
+        program = program.replace('"', '')
 
-    app_html = soup.find(type="application/ld+json")
-    app_json = (app_html.contents)[0]
-    json_for_mp4 = json.loads(app_json)
+        dict[week + " " + program] = link
 
-    mp4_source_link = json_for_mp4['embedUrl']
+    return dict
 
-    with open(str(key) + ".mp4", 'wb') as f_out:
-        r = requests.get(mp4_source_link, headers=headers, stream=True)
+""" recibe dict donde las claves son el nombre y semana del programa y el valor el link a la pagina"""
+def get_mp4_source_as_dict_values(dict):
+
+    for key in dict.keys():
+
+        r = requests.get(dict[key])
+
+        soup = BeautifulSoup(r.text, 'html.parser')
+
+        app_html = soup.find(type="application/ld+json")
+        app_json = (app_html.contents)[0]
+        json_for_mp4 = json.loads(app_json)
+
+        mp4_source_link = json_for_mp4['embedUrl']
+
+        dict[key] = mp4_source_link
+    
+    return dict
+
+""" recibe una lista de listas [[week, program, link]...]"""
+def transform(links_list):
+
+    dict = clean_list(links_list)
+
+    return get_mp4_source_as_dict_values(dict)
+
+""" recibe nombre a dar al archivo mp4 y link de source """
+def get_mp4_files(file_name, link):
+
+    headers = {'resolution':'720','User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36'}
+
+    with open(file_name + ".mp4", 'wb') as f_out:
+        r = requests.get(link, headers=headers, stream=True)
         print(r)
         for chunk in r.iter_content(chunk_size=1024*1024):
             if chunk:
                 f_out.write(chunk)
+
+""" recibe dict con claves nombre y semana programa, valor source mp4, los descarga"""
+def load(dict):
+
+    for key in dict.keys():
+
+        get_mp4_files(key,dict[key])
+
+""" funcion principal, recibe url local de archivo excel a consumir"""
+def main(local_url):
+
+    rows_list = extract(local_url)
+
+    programs_dict = transform(rows_list)
+
+    load(programs_dict)
+
+main(local_url)
